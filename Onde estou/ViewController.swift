666c04e4148892a1f9e35930dc901a9add281e71
Victor Rodrigues Novais <victor96novais@gmail.com>
2020-05-01 23:55:36 -0300

    poc

--- Onde estou/ViewController.swift
+++ Onde estou/ViewController.swift
@@ -9,13 +9,36 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController, MKMapViewDelegate {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
     @IBOutlet weak var mapa: MKMapView!
+    var gerenciadorLocalizacao = CLLocationManager()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        gerenciadorLocalizacao.delegate = self
+        gerenciadorLocalizacao.desiredAccuracy = kCLLocationAccuracyBest
+        gerenciadorLocalizacao.requestWhenInUseAuthorization()
+        gerenciadorLocalizacao.startUpdatingLocation()
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status != .authorizedWhenInUse {
+            var alertaController = UIAlertController(title: "Permissao de localização", message: "Necessário permissão para acesso à sua localização! por favor, habilite.", preferredStyle: .alert)
+            
+            var acaoConfiguracoes = UIAlertAction(title: "Abrir configurações", style: .default) { (alertaConfiguracoes) in
+                if let configuracoes = NSURL(string: UIApplication.openSettingsURLString) {
+                    
+                    UIApplication.shared.open(configuracoes as URL)
+                    
+                }
+            }
+            
+            var acaoCancelar = UIAlertAction(title: "Cancelar", style: .default, handler: nil)
+        }
     }
 
 
